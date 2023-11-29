bfb291b603466fb536afcdd162534f472d4168a9
jpalmer37 <john.palmer1288@gmail.com>
2023-11-29 11:05:10 -0800

    improved rename.py

---
+++ extra/rename.py
@@ -0,0 +1,54 @@
+#%%
+import os 
+import sys
+import pandas as pd 
+import argparse
+from glob import glob 
+
+
+def init_parser():
+	parser = argparse.ArgumentParser()
+	# parser.add_argument( 'metadata',  help='FASTA consensus sequence containing all query sequences. Segment MUST be included in the header.')	
+	parser.add_argument( 'fastq_path',  help='BLAST results from vsearch, containing Genbank seqs as queries against an annotated GISAID DB')	
+	parser.add_argument( '-x', '--execute', action='store_true', help='Run the renaming. Without this flag, will just perform dry runs.')
+	
+	return parser
+
+
+def rename(path, execute=False):
+	os.chdir(path)
+	
+	if execute:
+		print("Renaming files...")
+
+	folder_names = glob('*')
+
+	for filepath in glob("*/*.gz"):
+		fields = filepath.split("_")
+		folder_name, file_name = fields[0].split("/")
+		
+		newname = "_".join([folder_name, *fields[1:]])
+		#print("Starting to rename file")
+		print(filepath, " --> ", newname)
+
+
+		if execute:
+			os.rename(filepath, newname)
+
+	if execute:
+		print("Renamed all files. \nRemoving directories...")
+
+		for folder in folder_names:
+			os.rmdir(folder)
+		print("Complete.")
+
+	else:
+		print("DRY RUN ONLY.")
+	
+def main(args):
+	rename(args.fastq_path,  args.execute)
+	exit(0)
+if __name__ == '__main__':
+	parser = init_parser()
+	main(parser.parse_args())
+	
