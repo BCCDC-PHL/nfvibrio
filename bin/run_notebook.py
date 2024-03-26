e6d6a278fdad03f81f4d9353c1ba91b351679633
jpalmer37 <john.palmer1288@gmail.com>
2024-03-26 13:30:07 -0700

    fix run_notebook.py parameters

--- bin/run_notebook.py
+++ bin/run_notebook.py
@@ -7,7 +7,7 @@ CONFIG_PATH = os.path.dirname(__file__) + '/../assets/.ipynb.config'
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--notebook', required=True, help='Name of the Jupyter notebook to run')
-    parser.add_argument('-o', '--outname', help='Name of the output HTML report')
+    parser.add_argument('-o', '--output', help='Name of the output HTML report')
     parser.add_argument('-f', '--format', default='html', help='Name of the output HTML report')
     parser.add_argument('remainder', nargs=argparse.REMAINDER, help='All arguments to be passed to the Jupyter notebook')
     
@@ -21,7 +21,7 @@ def main():
 
     with open(CONFIG_PATH,'w') as f:
         f.write(' '.join([__file__] + args.remainder)+'\n')
-    output = f'--output {os.getcwd()}/{args.outname}' if args.outname else ''
+    output = f'--output {os.getcwd()}/{args.output}' if args.output else ''
     os.system(f'jupyter nbconvert --execute --no-input --to {args.format} {output} {args.notebook}')
     return None
 
